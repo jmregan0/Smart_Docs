02a72a532e2e9be0ccc47686a54971b6b182ae90
bryan <bryan@localhost.localdomain>
2017-06-22 12:07:25 -0500

    writes to firebase onChange

--- app/components/Editor.jsx
+++ app/components/Editor.jsx
@@ -6,23 +6,37 @@ export default class MyEditor extends React.Component {
   constructor(){
     super();
     this.state = {editorState: EditorState.createEmpty()};
-    this.onChange = editorState=>this.setState({editorState});
+    this.handleChange = this.handleChange.bind(this);
+  }
+ 
+  componentDidMount(){
+    //this.listenTo(this.props.fireRef)
+    console.log('props:',this.props);
+  }
+
+  handleChange(editorState){
+    let temp = JSON.stringify(editorState);
+    console.log("handleChange:",temp);
+    this.props.fireRef.set(temp);
+    this.setState({editorState},
+      ()=>console.log('state updated:',this.state.editorState.getCurrentContent().getPlainText()));
   }
 
   /*
-  componentDidMount(){
-    this.listenTo(this.props.fireRef)
-  }
-
   componentWillUnmount() {
     // When we unmount, stop listening.
     this.unsubscribe()
   }
+  */
 
+  /*
   componentWillReceiveProps(incoming, outgoing) {
     // When the props sent to us by our parent component change,
     // start listening to the new firebase reference.
-    this.listenTo(incoming.fireRef)
+
+    //this.listenTo(incoming.fireRef)
+
+    console.log("incoming:",incoming);
   }
 
   listenTo(fireRef) {
@@ -30,15 +44,19 @@ export default class MyEditor extends React.Component {
     if (this.unsubscribe) this.unsubscribe()
 
     // Whenever our ref's value changes, set {value} on our state.
-    const listener = fireRef.on('value', snapshot =>
-      this.setState({editorState: snapshot.val()}))
+    const listener = fireRef.on('value', snapshot => {
+      console.log('database update:',snapshot,snapshot.val());
+      this.setState({editorState: snapshot.val()})
+    });
 
     // Set unsubscribe to be a function that detaches the listener.
     this.unsubscribe = () => fireRef.off('value', listener)
   }
 
-  write = event => this.props.fireRef &&
-    this.props.fireRef.set(this.state.editorState.getCurrentContent().getPlainText())
+  write = event => {
+    let alias = this.state.editorState.getCurrentContent().getPlainText();
+    this.props.fireRef && this.props.fireRef.set(alias)
+  }
   */
 
   render(){
@@ -48,13 +66,9 @@ export default class MyEditor extends React.Component {
         <h1>EDITOR</h1>
         <Editor
           editorState={this.state.editorState}
-          onChange={this.onChange}
+          onChange={this.handleChange}
           />
       </div>
     )
   }
 }
-
-
-
-// console.log(this.state.editorState.getCurrentContent().getPlainText())
