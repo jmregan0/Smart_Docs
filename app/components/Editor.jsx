0c5a4234943047e49c562f47935c6697169d725c
Will McCracken <wem5637@gmail.com>
2017-06-22 10:59:10 -0500

    attempt at update to database

--- app/components/Editor.jsx
+++ app/components/Editor.jsx
@@ -9,15 +9,51 @@ export default class MyEditor extends React.Component {
     this.onChange = editorState=>this.setState({editorState});
   }
 
+  componentDidMount(){
+    this.listenTo(this.props.fireRef)
+  }
+
+  componentWillUnmount() {
+    // When we unmount, stop listening.
+    this.unsubscribe()
+  }
+
+  componentWillReceiveProps(incoming, outgoing) {
+    // When the props sent to us by our parent component change,
+    // start listening to the new firebase reference.
+    this.listenTo(incoming.fireRef)
+  }
+
+  listenTo(fireRef) {
+    // If we're already listening to a ref, stop listening there.
+    if (this.unsubscribe) this.unsubscribe()
+
+    // Whenever our ref's value changes, set {value} on our state.
+    const listener = fireRef.on('value', snapshot =>
+      this.setState({editorState: snapshot.val()}))
+
+    // Set unsubscribe to be a function that detaches the listener.
+    this.unsubscribe = () => fireRef.off('value', listener)
+  }
+
+  write = event => this.props.fireRef &&
+    this.props.fireRef.set(this.state.editorState.getCurrentContent().getPlainText())
+
+
   render(){
 
     return (
       <div>
-        <h1>EDITOR!!</h1>
+        <h1 onClick={(()=>{console.log(this.props.fireRef)})}>EDITOR!!</h1>
         <Editor
           editorState={this.state.editorState}
           onChange={this.onChange} />
+
       </div>
     )
   }
 }
+
+
+
+// console.log(this.state.editorState.getCurrentContent().getPlainText())
--- app/containers/EditorContainer.jsx
+++ app/containers/EditorContainer.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import Editor from '../components/Editor'
+import {Route} from 'react-router'
+import firebase from 'APP/fire'
+const db = firebase.database()
+import MyEditor from '../components/Editor'
 
-
-const mapState = (state) => {
-	return{
-
-  }
-}
-
-const mapDispatch = (dispatch) => {
-  return {
-
-  }
-}
-
-export default connect(mapState, mapDispatch)(Editor)
+// This component is a little piece of glue between React router
+// and our Scratchpad component. It takes in props.params.title, and
+// shows the Scratchpad along with that title.
+export default ({params: {title}}) =>
+  <div>
+    <h1>{title}</h1>
+    {/* Here, we're passing in a Firebase reference to
+        /scratchpads/$scratchpadTitle. This is where the scratchpad is
+        stored in Firebase. Each scratchpad is just a string that the
+        component will listen to, but it could be the root of a more complex
+        data structure if we wanted. */}
+    <MyEditor fireRef={db.ref('DraftJs').child(title)}/>
+  </div>
