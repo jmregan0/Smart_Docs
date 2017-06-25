8a5d1a1c03776a7bafa56856f9b0e7289ddf7fb8
Jacob Regan <jacobmregan0@gmail.com>
2017-06-24 21:11:20 -0500

    sets up parsing for sentiment and entity analysis. Hooks in functions that map over data and return meaningful results

--- app/components/Editor.jsx
+++ app/components/Editor.jsx
@@ -9,13 +9,59 @@ import firebase from 'APP/fire/index.js'
 export default class MyEditor extends React.Component {
   constructor(){
     super();
-    this.state = {editorState: EditorState.createEmpty()};
+    this.state = {
+      editorState: EditorState.createEmpty(),
+      nlpSentiment: null,
+      nlpEntity: null,
+      nlpRelationships: null
+    };
     this.onChange = editorState=>this.setState({editorState});
-    this.findResources = this.findResources.bind(this);
+    this.findEntity = this.findEntity.bind(this);
+    this.findSentiment = this.findSentiment.bind(this);
+    this.findRelationships = this.findRelationships.bind(this);
     this.db = firebase.database();
   }
 
-  findResources(text){
+  executeEntityAnalysis(){
+    let mainEntities = [];
+    this.state.nlpEntity.forEach(entity => {
+      mainEntities.push(entity.normalized)
+    })
+    console.log('some the key words in this text are: ', mainEntities)
+  }
+
+  executeSentimentAnalysis(){
+    let places = [];
+    let people = [];
+    let organizations = [];
+    let overallSentiment;
+    let positive = 0;
+    let negative = 0;
+    let neutral = 0;
+    this.state.nlpSentiment.forEach(function(word){
+      if(word.type === 'PERSON'){
+        people.push(word.normalized)
+        word.sentiment.label === 'pos' ? positive ++ : word.sentiment.label === 'neg' ? negative ++ : neutral ++
+      } else if(word.type === 'LOCATION'){
+        places.push(word.normalized)
+        word.sentiment.label === 'pos' ? positive ++ : word.sentiment.label === 'neg' ? negative ++ : neutral ++
+      } else if(word.type === 'ORGANIZATION'){
+        organizations.push(word.normalized)
+        word.sentiment.label === 'pos' ? positive ++ : word.sentiment.label === 'neg' ? negative ++ : neutral ++
+      }
+    })
+    if(positive > negative && positive > neutral){
+      overallSentiment = 'positive'
+    } else if (negative > positive && negative > neutral){
+      overallSentiment = 'negative'
+    } else {
+      overallSentiment = 'neutral'
+    }
+    console.log('results were: places', places, "people", people, "organizations", organizations, "sentiment", overallSentiment)
+    console.log('sentiments', positive, negative, neutral)
+  }
+
+  findEntity(text){
     return axios({
       method: 'post',
       url: 'http://localhost:3000/api/analyze/entity',
@@ -23,12 +69,41 @@ export default class MyEditor extends React.Component {
         text: text
       }
     })
+    .then(result => result)
     .then(result => {
       console.log('sent our text to nlp and back again!', result.data.entities)
+      this.setState({nlpEntity: result.data.entities})
     })
   }
 
+  findSentiment(text){
+      return axios({
+        method: 'post',
+        url: 'http://localhost:3000/api/analyze/sentiment',
+        data:{
+          text: text
+        }
+      })
+      .then(result => result)
+      .then(result => {
+        console.log('sent our text to nlp and back again!', result.data)
+        this.setState({nlpSentiment: result.data.entities})
+      })
+    }
 
+  findRelationships(text){
+      return axios({
+        method: 'post',
+        url: 'http://localhost:3000/api/analyze/relationships',
+        data:{
+          text: text
+        }
+      })
+      .then(result => result)
+      .then(result => {
+        console.log('sent our text to nlp and back again!', result.data)
+      })
+    }
 
   render(){
 
@@ -38,7 +113,14 @@ export default class MyEditor extends React.Component {
         <Editor
           editorState={this.state.editorState}
           onChange={this.onChange} />
-          <button onClick={() => this.findResources(this.state.editorState.getCurrentContent().getPlainText())}>ANALYZE</button>
+          <button onClick={() => this.findSentiment(this.state.editorState.getCurrentContent().getPlainText())}>Parse for Sentiment</button>
+          <button onClick={() => this.executeSentimentAnalysis()}>Show me facts and sentiment analysis</button>
+          <br/>
+          <button onClick={() => this.findEntity(this.state.editorState.getCurrentContent().getPlainText())}>Parse for Entity</button>
+          <button onClick={() => this.executeEntityAnalysis()}>Show me Entity Analysis</button>
+          <br/>
+          <button>button</button>
+          <button>button</button>
       </div>
     )
   }
