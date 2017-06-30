e1db25f24f8ff008f472bb6764a3e5c75bebcedb
Jacob Regan <jacobmregan0@gmail.com>
2017-06-30 16:32:39 -0500

    fixes sidebar on sentiment view

--- app/components/Sentiment.jsx
+++ app/components/Sentiment.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios'
-import SentimentometerEntity from '../components/SentimentometerEntity'
+import { Link } from 'react-router'
 
 const Sentiment = (props) => {
   console.log('sentimentResults', props.sentimentResults)
@@ -122,39 +122,7 @@ const Sentiment = (props) => {
             <div className="collapse navbar-collapse navbar-ex1-collapse">
                 <ul className="nav navbar-nav side-nav">
                     <li>
-                        <a href="index.html"><i className="fa fa-fw fa-dashboard"></i> Dashboard</a>
-                    </li>
-                    <li>
-                        <a href="charts.html"><i className="fa fa-fw fa-bar-chart-o"></i> Charts</a>
-                    </li>
-                    <li className="active">
-                        <a href="tables.html"><i className="fa fa-fw fa-table"></i> Tables</a>
-                    </li>
-                    <li>
-                        <a href="forms.html"><i className="fa fa-fw fa-edit"></i> Forms</a>
-                    </li>
-                    <li>
-                        <a href="bootstrap-elements.html"><i className="fa fa-fw fa-desktop"></i> Bootstrap Elements</a>
-                    </li>
-                    <li>
-                        <a href="bootstrap-grid.html"><i className="fa fa-fw fa-wrench"></i> Bootstrap Grid</a>
-                    </li>
-                    <li>
-                        <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i className="fa fa-fw fa-arrows-v"></i> Dropdown <i className="fa fa-fw fa-caret-down"></i></a>
-                        <ul id="demo" className="collapse">
-                            <li>
-                                <a href="#">Dropdown Item</a>
-                            </li>
-                            <li>
-                                <a href="#">Dropdown Item</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="blank-page.html"><i className="fa fa-fw fa-file"></i> Blank Page</a>
-                    </li>
-                    <li>
-                        <a href="index-rtl.html"><i className="fa fa-fw fa-dashboard"></i> RTL Dashboard</a>
+                        <Link to="research"><i className="fa fa-fw fa-chevron-left"></i> Back to Dashboard</Link>
                     </li>
                 </ul>
             </div>
