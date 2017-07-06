4230ecafd2594544df2f5a94fb8a127d47f9c4c2
bryan <bryan@localhost.localdomain>
2017-07-06 09:32:35 -0500

    adds wiki text to tooltips for peer view

--- app/draftjsscratchpad/draftDecorator.js
+++ app/draftjsscratchpad/draftDecorator.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Encoder} from 'node-html-encoder';
+import axios from 'axios';
+import ReactHtmlParser from 'react-html-parser';
 const {EditorState,convertToRaw,convertFromRaw} = require('draft-js');
 
 
@@ -27,7 +29,35 @@ module.exports.entitySpan = props => (
 class entitySpan extends React.Component {
   constructor(){
     super();
-    this.state = {};
+    this.state = {
+      tooltipText: 'Loading...',
+    };
+
+    this.wikiSearch = this.wikiSearch.bind(this);
+  }
+
+  wikiSearch(entity){
+    let encoder = new Encoder('entity');
+    //const SEARCHURL = 'http://localhost:3000/api/wikipedia/search';
+    const SEARCHURL = 'http://web02.com:3000/api/wikipedia/search';
+
+    // encode non-ascii characters
+    let query = encoder.htmlEncode(entity);
+    // replace whitespace with '%20'
+    query = query.replace(/\s/,'%20');
+
+    return axios.post(SEARCHURL,{tag:query})
+    .then(res => res.data)
+  }
+
+  componentDidMount(){
+    this.wikiSearch(this.props.decoratedText)
+    .then(searchResults=>{
+      console.log('wikisearch results:',searchResults);
+      let parsed = ReactHtmlParser(searchResults.search[0].snippet);
+      this.setState({tooltipText: parsed});
+    })
+    .catch(error=>console.error('wikisearch error:',error));
   }
 
   render(){
@@ -39,7 +69,7 @@ class entitySpan extends React.Component {
         data-offset-key={this.props.offsetKey}
       >
         {this.props.children}
-        <span className="tooltiptextCustom">Tooltip text</span>
+        <span className="tooltiptextCustom">{this.state.tooltipText}</span>
       </div>
     );
   }
